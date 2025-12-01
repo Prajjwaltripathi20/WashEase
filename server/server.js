806feea546f12a7f5ffff94bc35e0e6d625cb943
Prajjwaltripathi20 <Connectwithprajjwal@gmail.com>
2025-12-02 00:48:02 +0530

    database

--- server/server.js
+++ server/server.js
@@ -18,7 +18,14 @@ connectDB().then((connected) => {
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+  );
+  
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
